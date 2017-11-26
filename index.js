acb24ab8cf8dca43b2caca32be7220edbd66e8e8
Ashley Lv <ashley_lyf@icloud.com>
2017-11-26 11:57:37 +0800

    Demo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import VNode from './src/vnode'
 
-var children = [new VNode('h3',1,{'data-title':'title1'},null,[new VNode(null,3,null,'title',null)]),new VNode('div',1,{'style':{'background':'#ea8e8e','width':'100px','height':'100px'}},null,null)]
-var nodes = new VNode('div',1,null,null,children)
-nodes.render()
+var children = [new VNode('h3',1,{'data-title':'title1','width':'300'},'vnode-title',[new VNode(null,3,null,'This is title')]),new VNode('div',1,{'style':{'background':'#ea8e8e','width':'100px','height':'100px'}},null,null),new VNode(null,8,null,'comments')]
+var nodes = new VNode('div',1,{'data-org':'test'},null,children)
+var el =nodes.render()
+document.body.appendChild(el)
+
+
 console.log(nodes)
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,6 @@ module.exports = {
       },
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin()
+        // new webpack.optimize.UglifyJsPlugin()
     ],
 };
