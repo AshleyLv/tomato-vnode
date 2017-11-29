c789844abddd1e59be8cfe265cf1ea3292585609
Ashley Lv <ashley_lyf@icloud.com>
2017-11-29 22:17:49 +0800

    Diff

--- src/diff.js
+++ src/diff.js
@@ -7,27 +7,27 @@ const PROP = 2
 
 export default function diff(oldVNode, newVNode){
     let index = 0
-    let patches = []
-    diffVNode(oldVNode,newVNode,index,patches)
-    return patches
+    let directives = []
+    diffVNode(oldVNode,newVNode,index,directives)
+    return directives
 
 }
 
-function diffVNode(oldVNode,newVNode,index,patches){
+function diffVNode(oldVNode,newVNode,index,directives){
     var patch = []
 
     if(newVNode && isSomeTypeNode(oldVNode,newVNode)){
         if(newVNode.nodeType===3 || newVNode.nodeType===8){
             if(oldVNode.text !== newVNode.text){
-                patch.push({type:TEXT, content: newVNode})
+                directives.push({type:TEXT, content: newVNode})
             }
         } else if(newVNode.nodeType===1){
             if(oldVNode.tag === newVNode.tag && oldVNode.key == newVNode.key){
                 var propPatches = diffProps(oldVNode.props, newVNode.props)
                 if(Object.keys(propPatches).length>0){
-                    patch.push({type:PROP, content: propPatches})
+                    directives.push({type:PROP, content: propPatches})
                 }
-                diffChildren(oldVNode.children,newVNode.children)
+                diffChildren(oldVNode.children,newVNode.children,directives)
             }
         }
     }
@@ -49,6 +49,26 @@ function diffProps(oldProps,newProps){
     return patches
 }
 
-function diffChildren(oldChildren,newChildren){
+function diffChildren(oldChildren,newChildren,directives){
+    let oldKeyIndexObject = parseNodeList(oldChildren)
+    let newKeyIndexObject = parseNodeList(newChildren)
+    let sameItems = []
+    
+    for(let key in oldKeyIndexObject){
+        if(newKeyIndexObject.hasOwnProperty(key)){
+            sameItems.push({'oldIndex':oldKeyIndexObject[key],'newIndex':newKeyIndexObject[key],'node':newChildren[newKeyIndexObject[key]]})
+        }
+    }
+}
+
+function parseNodeList(nodeList){
+    let keyIndex = {}
+    nodeList.forEach((item,i)=>{
+        if(item.key){
+            keyIndex[item.key] = i
+        }
+       
+    })
+    return keyIndex
 
 }
